4908c11ca4783bbb7536723e9020d0d287b959bc
yeexiong <yee.l.xiong@gmail.com>
2026-04-20 17:16:27 -0500

    update

--- backend/board/admin.py
+++ backend/board/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import Status, Project, Objective, Task, Comment
-
-admin.site.register(Status)
-admin.site.register(Project)
-admin.site.register(Objective)
-admin.site.register(Task)
-admin.site.register(Comment)
