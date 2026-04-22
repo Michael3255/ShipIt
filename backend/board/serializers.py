22ec0b9b0b19098968cfd69a1542b94eb7a4694b
Judy <hegao906@gmail.com>
2026-04-21 22:50:07 -0400

    test2

--- backend/board/serializers.py
+++ backend/board/serializers.py
@@ -1,9 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Project, Objective, Task, Comment
-
-User = get_user_model()
-
+from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
     owner = serializers.StringRelatedField(read_only=True)
