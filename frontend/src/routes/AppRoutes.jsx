0a41fc11887bbfa8f4fb0a104a51b50b064d2215
Michael3255 <mclcollege1@gmail.com>
2026-04-25 17:41:41 -0400

    added objectives page, tasks page, comments on a task page, and all of their required api files.  also updated approutes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 import { ProjectsPage } from '../pages/ProjectsPage'
+import { ObjectiveDetail } from '../pages/ObjectiveDetail'
+import { TasksPage } from '../pages/TasksPage'
+import { TaskDetail } from '../pages/TaskDetail'
 
 function AppRoutes() {
     return (
@@ -11,6 +14,15 @@ function AppRoutes() {
 
                     {/* Projects Page routes */}
                     <Route path="/projects" element={<ProjectsPage />} />
+
+                    {/* Objectives Page routes */}
+                    <Route path="/projects/:id" element={<ObjectiveDetail />} />
+
+                    {/* Tasks Page routes */}
+                    <Route path="/objectives/:id" element={<TasksPage />} />
+
+                    {/* Tasks Detail routes (comments on a task) */}
+                    <Route path="/tasks/:id" element={<TaskDetail />} />
                 </Routes>
             
             </AuthProvider> 
