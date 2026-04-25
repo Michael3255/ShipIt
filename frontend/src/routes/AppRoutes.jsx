e5e01f598d9710fa892c055994829d0b9a1b796d
Judy <hegao906@gmail.com>
2026-04-25 16:15:33 -0400

    Fix AppRoutes routing and resolve updates

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,8 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
+
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import { ProjectsPage } from "../pages/ProjectsPage";
+import { TeamsPage } from "../pages/TeamsPage";
 
 function AppRoutes() {
     return (
@@ -17,6 +19,9 @@ function AppRoutes() {
 
                 {/* Projects */}
                 <Route path="/projects" element={<ProjectsPage />} />
+
+                {/* Teams */}
+                <Route path="/teams" element={<TeamsPage />} />
             </Routes>
         </AuthProvider>
     );
