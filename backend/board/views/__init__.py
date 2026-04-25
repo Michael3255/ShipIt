8c2a41f4b1c51c20cd386843c27cf8a140878d10
Michael3255 <mclcollege1@gmail.com>
2026-04-25 10:30:26 -0400

    added init file in board views

---
+++ backend/board/views/__init__.py
@@ -0,0 +1,4 @@
+from .project_views import ProjectListCreateView, ProjectDetailView
+from .objective_views import ObjectiveListCreateView, ObjectiveDetailView
+from .task_views import TaskListCreateView, TaskDetailView
+from .comment_views import CommentListCreateView, CommentDetailView
