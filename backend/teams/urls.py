6cee99472f30f9ab626484793307fb396236fa44
VaNiesha Honani <vhonani77@gmail.com>
2026-05-02 13:46:54 -0700

    more stuff

--- backend/teams/urls.py
+++ backend/teams/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import TeamListCreateView,TeamDetailView 
 
 urlpatterns = [
-    path('team_list_create/', TeamListCreateView.as_view(), name='team_list_create'),
-    path('<int:pk>/', TeamDetailView.as_view(), name='team_detail'),
+    path('teams/', TeamListCreateView.as_view(), name='team_list_create'),
+    path('teams/<int:pk>/', TeamDetailView.as_view(), name='team_detail'),
 ]
--- backend/teams/views.py
+++ backend/teams/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from .models import Team
 from .serializers import TeamSerializer
 
 
 class TeamListCreateView(generics.ListCreateAPIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
