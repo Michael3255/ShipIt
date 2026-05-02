d0a283161c9726023355638fa392985d5dd5fa84
VaNiesha Honani <vhonani77@gmail.com>
2026-05-02 13:52:24 -0700

    deleted the change for isAuthenticatedAllowAny

--- backend/teams/views.py
+++ backend/teams/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
 from .models import Team
 from .serializers import TeamSerializer
 
 
 class TeamListCreateView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
