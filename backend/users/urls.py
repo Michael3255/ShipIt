46028af35ff8ca492a8d649ea797bcdb6c12c593
Michael3255 <mclcollege1@gmail.com>
2026-05-14 03:13:58 -0400

    troubleshoot 6

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RegisterView, TeamMembersView
 
 urlpatterns = [
-    path('users/signup/', RegisterView.as_view(), name='create_user'),
-    path('users/team-members/', TeamMembersView.as_view(), name='team_members'),
+    path('signup/', RegisterView.as_view(), name='create_user'),
+    path('team-members/', TeamMembersView.as_view(), name='team_members'),
 ]
